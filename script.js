12c3a2cf090b32a3a7a1816dc9dd4f6eb05bf804
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-05-23 15:15:07 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function add(){
+    document.querySelector("#number1").innerText++;
+    
+}
+function add2(){
+    document.querySelector("#number2").innerText++;
+    
+}
+
+function add3(){
+    document.querySelector("#number3").innerText++;
+    
+}
